35a833a5c2edd0c9c5613ffc2687a982a925bc2e
liubin2022 <thoughtliu@126.com>
2022-02-14 23:01:16 +0800

    【修改】testcase01

--- TestCase/Application01/TestCase01.py
+++ TestCase/Application01/TestCase01.py
@@ -14,6 +14,8 @@ class TestCase01(object):
     def aaa(self):
         dic = {}  # 定义一个字
         dic['b'] = 3  # 在 dic 中加一条元素，key 为 b
+        print(dic)
+        print(dic.values())
         print(dic.keys())  # 先将 dic 的 key 打印出来，有一个元素 b
         exec("a = 4", dic)  # 在 exec 执行的语句后面跟一个作用域 dic
         print(dic.keys())  # exec 后，dic 的 key 多了一个
@@ -25,3 +27,5 @@ if __name__ == '__main__':
     print(dic.keys())  # 先将 dic 的 key 打印出来，有一个元素 b
     exec("a = 4", dic)  # 在 exec 执行的语句后面跟一个作用域 dic
     print(dic.keys())  # exec 后，dic 的 key 多了一个
+    aa = TestCase01()
+    aa.aaa()
