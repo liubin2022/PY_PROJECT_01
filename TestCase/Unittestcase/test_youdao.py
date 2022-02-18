a75265d87d217a6373a8451c39c5582a88169524
liubin2022 <thoughtliu@126.com>
2022-02-19 00:39:10 +0800

    【修改】test_youdao

---
+++ TestCase/Unittestcase/test_youdao.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Created on 2022年1月24日
+@author: LiuBin
+@note: Unittest testcase
+"""
+from selenium import webdriver
+import unittest, time
+from selenium.webdriver.common.by import By
+
+
+class YoudaoTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(3)
+        self.base_url = "http://www.youdao.com"
+
+    def test_youdao(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element(By.XPATH, '//input[@id="search_input"]').clear()
+        driver.find_element(By.XPATH, '//input[@id="search_input"]').send_keys(u"你好")
+        # driver.find_element(By.XPATH, '//a[@class="translate_btn"]').click()
+        driver.find_element(By.CLASS_NAME, 'translate_btn').click()
+        time.sleep(3)
+        page_source = driver.page_source
+        self.assertIn("hello", page_source)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
