7b67d93a535c4250779d34ff4be3cd7eb21399a0
liubin2022 <thoughtliu@126.com>
2022-02-19 00:56:57 +0800

    【修改】百度

--- TestCase/Unittestcase/test_baidu.py
+++ TestCase/Unittestcase/test_baidu.py
@@ -3,10 +3,11 @@
 """
 Created on 2022年1月24日
 @author: LiuBin
-@note: youdao.com
+@note: baidu.com
 """
+import time
+import unittest
 from selenium import webdriver
-import unittest, time
 from selenium.webdriver.common.by import By
 
 
@@ -14,18 +15,17 @@ class YoudaoTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(3)
-        self.base_url = "http://www.youdao.com"
+        self.base_url = "http://www.baidu.com"
 
     def test_youdao(self):
         driver = self.driver
         driver.get(self.base_url + "/")
-        driver.find_element(By.XPATH, '//input[@id="search_input"]').clear()
-        driver.find_element(By.XPATH, '//input[@id="search_input"]').send_keys(u"你好")
-        # driver.find_element(By.XPATH, '//a[@class="translate_btn"]').click()
-        driver.find_element(By.CLASS_NAME, 'translate_btn').click()
+        driver.find_element(By.XPATH, '//input[@id="kw"]').clear()
+        driver.find_element(By.XPATH, '//input[@id="kw"]').send_keys(u"你好")
+        driver.find_element(By.ID, 'su').click()
         time.sleep(3)
         page_source = driver.page_source
-        self.assertIn("hello", page_source)
+        self.assertIn("你好，是一个汉语词语", page_source)
 
     def tearDown(self):
         self.driver.quit()
